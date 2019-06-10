438b5b8b33a5e77d5731b634c71ded77d1fbfb8d
markovac <tm50053@fer.hr>
2019-06-10 22:21:07 +0200

    Prosiranje skripte corrector.sh

--- zavrsni/corrector.sh
+++ zavrsni/corrector.sh
@@ -12,11 +12,38 @@ do
 	HOSTDIR=$TESTDIR/${test}/hosts/
 	for conf in `ls $HOSTDIR`
 	do
-		target=$HOSTDIR/${conf}/etc/strongswan.conf
+		target=$HOSTDIR${conf}/etc/strongswan.conf
 		if [ -e $target ]
 		then
-			echo $target
-			cat $target | sed 's/load/load_modular/g' | cat > $target
+			#  load -> load_modluar
+			grep -E "\bload\b" $target > /dev/null 2>&1
+			if [ $? -eq 0 ]
+			then
+				sed -i bak -e 's/load/load_modular/g' $target
+			fi
+			
 		fi
+		
+		target=$TESTDIR/${test}/evaltest.dat
+		if [ -e $target ]
+		then
+			# icmp_seq=1 -> icmp_seq=
+			grep -E "\bicmp_.eq=1" $target > /dev/null 2>&1
+			if [ $? -eq 0 ]
+			then
+				sed -i bak -e 's/icmp_.eq=1/icmp_.eq=/g' $target
+			fi
+			
+			# symbolic -> IP
+			grep -E "tcpdump" $target > /dev/null 2>&1
+			if [ $? -eq 0 ]
+			then
+				sed -i bak -e 's/ moon.strongswan.org / 192.168.0.1 /g' $target
+				sed -i bak -e 's/ moon.strongswan.org:/ 192.168.0.1:/g' $target
+				sed -i bak -e 's/ sun.strongswan.org / 192.168.0.2 /g' $target
+				sed -i bak -e 's/ sun.strongswan.org:/ 192.168.0.2:/g' $target				
+			fi
+		fi
+		
 	done
 done
